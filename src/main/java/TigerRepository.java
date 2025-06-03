2bfa09ae2af53b136176b2f0a715b1647c05ecf6
jacob <J_echeverry@uncg.edu>
2025-06-02 23:31:51 -0400

    Created Tiger repository with region, subspecies, and name search queries

---
+++ src/main/java/TigerRepository.java
@@ -0,0 +1,18 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface TigerRepository extends JpaRepository<tiger,Long> {
+    List<tiger> getTigersByHabitatRegion(String habitatRegion);
+
+    @Query(value = "SELECT * From tigers t where t.subspecies = ?1" , nativeQuery = true)
+    List<tiger> getTigersBySubspecies(String subspecies);
+
+    @Query(value = "SELECT * FROM tigers t WHERE t.name LIKE %?1%", nativeQuery = true)
+    List<tiger> getTigersByName(String name);
+}
+
